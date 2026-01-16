2dbf85ebddadcd8bf49d80714135c24e36a113ba
Siron <sironmudalige@gmail.com>
2026-01-16 16:06:25 +0530

    Change the script to store loggs

--- linux/backup.sh
+++ linux/backup.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
 SOURCE_DIR="$1"
-BACKUP_DIR="$HOME/BACKUPS"
+BACKUP_DIR="$HOME/devops-lab/project_1/backups"
 
 mkdir -p "$BACKUP_DIR"
-
-TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
-BACKUP_FILE="$BACKUP_DIR/backup_$TIMESTAMP.tar.gz"
-
-tar -czf "$BACKUP_FILE" "$SOURCE_DIR"
 
 if [ -z "$SOURCE_DIR" ]; then
   echo "Usage: ./backup.sh <drectory-to-backup>"
@@ -20,3 +15,15 @@ if [ ! -d "$SOURCE_DIR" ]; then
   exit 1
 fi
 
+TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
+BACKUP_FILE="$BACKUP_DIR/backup_$TIMESTAMP.tar.gz"
+
+tar -czf "$BACKUP_FILE" "$SOURCE_DIR"
+
+LOG_FILE="$BACKUP_DIR/backup.log"
+
+if [ $? -eq 0 ]; then
+  echo "$(date) | SUCCESS | $BACKUP_FILE" | tee -a "$LOG_FILE"
+else
+  echo "$(date) | FAILURE | Backup failed" | tee -a "$LOG_FILE"
+fi
